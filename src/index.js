38f207809ff8ea941fcff179ec4b0a4b27dbfb35
singh-ravi-siso <ravi16iiitg@gmail.com>
2021-06-13 21:30:36 +0530

    fix dependencies

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 const loaderUtils = require("loader-utils");
-
 const babel = require("@babel/core");
 
 function loader(source){
     const options = loaderUtils.getOptions(this);
-
 
     const transformed = babel.transform(source, {
         plugins: [["transform-remove-console", options]]
